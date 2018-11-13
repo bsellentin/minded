85d7f1db5c6f3e44d893448fa6f2cbc6ce8a7fbc
bsellentin <sel@gge-em.org>
2018-11-13 18:35:10 +0100

    bind filechooserbuttons to settings

--- minded/preferences.py
+++ minded/preferences.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''Preferences Dialog for MindEd'''
+'''
+Preferences Dialog for MindEd
+'''
 
-import os
+from gettext import gettext as _
 import logging
 
 import gi
@@ -14,7 +15,9 @@ from gi.repository import Gtk, Gio, Pango
 LOGGER = logging.getLogger(__name__)
 
 class PreferencesDialog():
-
+    '''
+    The preferences dialog window
+    '''
     def __init__(self, application):
 
         self.app = application
@@ -26,62 +29,70 @@ class PreferencesDialog():
         self.window.set_application(application)
 
         # look for settings
-        srcdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-        LOGGER.debug('srcdir: {}'.format(srcdir))
-        if os.path.exists(os.path.join(srcdir, 'data')):
-            LOGGER.warning('Running from source tree, using local settings')
-            schema_source = Gio.SettingsSchemaSource.new_from_directory(
-                os.path.join(srcdir, 'data'), Gio.SettingsSchemaSource.get_default(), False)
-            schema = Gio.SettingsSchemaSource.lookup(
-                schema_source, 'org.gge-em.MindEd', False)
-            LOGGER.debug('Gsettings schema: {}'.format(schema.get_path()))
-            if not schema:
-                raise Exception("Cannot get GSettings schema")
-            settings = Gio.Settings.new_full(schema, None, None)
-        else:
-            settings = Gio.Settings('org.gge-em.MindEd')
-
-        # Editor
-        settings.bind('fontname', builder.get_object('font_button'),
-                      'font-name', Gio.SettingsBindFlags.DEFAULT)
-        settings.bind('linenumbers', builder.get_object('display_line_numbers_checkbutton'),
-                      'active', Gio.SettingsBindFlags.DEFAULT)
-        settings.bind('showrightmargin', builder.get_object('right_margin_checkbutton'),
-                      'active', Gio.SettingsBindFlags.DEFAULT)
-        settings.bind('setrightmargin', builder.get_object('right_margin_position_spinbutton'),
-                      'value', Gio.SettingsBindFlags.DEFAULT)
-        settings.bind('autoindent', builder.get_object('auto_indent_checkbutton'),
-                      'active', Gio.SettingsBindFlags.DEFAULT)
-        settings.bind('indentontab', builder.get_object('indent_on_tab_checkbutton'),
-                      'active', Gio.SettingsBindFlags.DEFAULT)
-        settings.bind('tabwidth', builder.get_object('tabs_width_spinbutton'),
-                      'value', Gio.SettingsBindFlags.DEFAULT)
-        settings.bind('spacesinsteadtab', builder.get_object('insert_spaces_checkbutton'),
-                      'active', Gio.SettingsBindFlags.DEFAULT)
-        settings.bind('highlightcurrentline', builder.get_object('highlight_current_line_checkbutton'),
-                      'active', Gio.SettingsBindFlags.DEFAULT)
-        settings.bind('highlightmatchingbrackets', builder.get_object('bracket_matching_checkbutton'),
-                      'active', Gio.SettingsBindFlags.DEFAULT)
-        #smartbackspace
+        setters = [
+            # Editor
+            ['fontname', 'font_button', 'font-name'],
+            ['linenumbers', 'display_line_numbers_checkbutton', 'active'],
+            ['showrightmargin', 'right_margin_checkbutton', 'active'],
+            ['setrightmargin', 'right_margin_position_spinbutton', 'value'],
+            ['autoindent', 'auto_indent_checkbutton', 'active'],
+            ['indentontab', 'indent_on_tab_checkbutton', 'active'],
+            ['tabwidth', 'tabs_width_spinbutton', 'value'],
+            ['spacesinsteadtab', 'insert_spaces_checkbutton', 'active'],
+            ['highlightcurrentline', 'highlight_current_line_checkbutton', 'active'],
+            ['highlightmatchingbrackets', 'bracket_matching_checkbutton', 'active'],
+            #smartbackspace
+            # EVC
+            ['armgcc', 'gccfile', 'text'],
+            ['ldflags', 'ldflagsfile', 'text'],
+            ['incs', 'incsfile', 'text'],
+            ['armgplusplus', 'cplusfile', 'text'],
+            ['cplusplus', 'c++-compiler', 'active'],
+            # NXC
+            ['nbcpath', 'nbcexecfile', 'text'],
+            ['enhancedfw', 'use_enhanced_firmware_checkbutton', 'active']
+        ]
+        for setter in setters:
+            self.app.settings.bind(setter[0], builder.get_object(setter[1]),
+                                   setter[2], Gio.SettingsBindFlags.DEFAULT)
 
         wrap_mode_checkbutton = builder.get_object('line_wrap_mode_checkbutton')
-        wrap_mode_checkbutton.connect('toggled', self.on_wrap_mode_toggled, settings)
-        if settings.get_enum('linewrapmode'):
+        wrap_mode_checkbutton.connect('toggled', self.on_wrap_mode_toggled, self.app.settings)
+        if self.app.settings.get_enum('linewrapmode'):
             wrap_mode_checkbutton.set_active(True)
 
-        # EVC
-        cchooser = builder.get_object('choosec')
-        cchooser.set_current_folder('/usr/bin/')
-        settings.bind('armgcc', builder.get_object('cexecfile'),
-                      'text', Gio.SettingsBindFlags.DEFAULT)
-        settings.bind('armgplusplus', builder.get_object('cplusexecfile'),
-                      'text', Gio.SettingsBindFlags.DEFAULT)
-        settings.bind('cplusplus', builder.get_object('c++-compiler'),
-                      'active', Gio.SettingsBindFlags.DEFAULT)
-
         # NXC
-        settings.bind('enhancedfw', builder.get_object('use_enhanced_firmware_checkbutton'),
-                      'active', Gio.SettingsBindFlags.DEFAULT)
+        nbcfile = Gio.File.new_for_path(self.app.settings.get_string('nbcpath'))
+        if nbcfile.query_exists():
+            builder.get_object('choosenbc').set_file(nbcfile)
+        builder.get_object('choosenbc').set_current_folder_file(nbcfile)
+        builder.get_object('choosenbc').set_title(_('choose NBC-file'))
+
+        #EVC
+        gccfile = Gio.File.new_for_path(self.app.settings.get_string('armgcc'))
+        if gccfile.query_exists():
+            builder.get_object('choosegcc').set_file(gccfile)
+        builder.get_object('choosegcc').set_current_folder_file(gccfile)
+        builder.get_object('choosegcc').set_title(_('choose gcc-file'))
+
+        cplusfile = Gio.File.new_for_path(self.app.settings.get_string('armgplusplus'))
+        if cplusfile.query_exists():
+            builder.get_object('choosecplus').set_file(cplusfile)
+        builder.get_object('choosecplus').set_current_folder_file(cplusfile)
+        builder.get_object('choosecplus').set_title(_('choose C++-file'))
+
+        libev3file = Gio.File.new_for_path(self.app.settings.get_string('ldflags') + '/libev3api.a')
+        if libev3file.query_exists():
+            builder.get_object('chooseldflags').set_file(libev3file)
+        builder.get_object('chooseldflags').set_current_folder_file(libev3file)
+        builder.get_object('chooseldflags').set_title(_('choose libev3api-file'))
+
+        headerfile = Gio.File.new_for_path(self.app.settings.get_string('incs') + '/ev3.h')
+        if headerfile.query_exists():
+            builder.get_object('chooseincs').set_file(headerfile)
+        builder.get_object('chooseincs').set_current_folder_file(headerfile)
+        builder.get_object('chooseincs').set_title(_('choose ev3-header'))
+
 
         self.window.show_all()
         self.window.connect('delete-event', self.quit)
@@ -98,7 +109,6 @@ class PreferencesDialog():
         else:
             settings.set_string('linewrapmode', 'none')
 
-
     def on_compiler_toggled(self, button):
         if button.get_active():
             state = "on"
@@ -107,6 +117,24 @@ class PreferencesDialog():
             state = "off"
         LOGGER.debug("Button {} was turned {}".format(button.get_label(), state))
 
+    def on_cplusfile_set(self, widget):
+        LOGGER.debug('C++-compiler: {}'.format(widget.get_file().get_path()))
+        self.app.settings.set_string('armgplusplus', widget.get_file().get_path())
+
+    def on_incfolder_set(self, widget):
+        LOGGER.debug('header: {}'.format(widget.get_file().get_parent().get_path()))
+        self.app.settings.set_string('incs', widget.get_file().get_parent().get_path())
+
+    def on_ldfile_set(self, widget):
+        LOGGER.debug('libev3: {}'.format(widget.get_file().get_parent().get_path()))
+        self.app.settings.set_string('ldflags', widget.get_file().get_parent().get_path())
+
+    def on_cfile_set(self, widget):
+        LOGGER.debug('arm-gcc: {}'.format(widget.get_file().get_path()))
+        self.app.settings.set_string('armgcc', widget.get_file().get_path())
+
+    def on_nbcfile_set(self, widget):
+        self.app.settings.set_string('nbcpath', widget.get_file().get_path())
 
     def quit(self, *args):
         '''Close preferences-dialog'''
